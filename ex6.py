0d6eac6f62a3f4a1f37fdd23d8aa3553d83117d7
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-28 23:42:20 -0700

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,34 @@
+# store 10 in the variable types_of_people
+types_of_people = 10
+# store the f-string in variable x
+x = f"There are {types_of_people} types of people."
+
+#store the 'binary' in the variable binary
+binary = "binary"
+# store 'don't' in variable do_not
+do_not = "don't"
+# store f-string in variable y
+y = f"Those who know {binary} and those who {do_not}."
+
+
+print(x) # print the value of x
+print(y) # print the value of y
+
+print(f"I said: {x}") # print 'I said: + value in variable x
+print(f"I also said: '{y}'") # print 'I also said: + value in variable y
+
+# store the boolean 'False' in variable hilarious
+hilarious = False
+# store string in variable joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious)) # print and format joke_evaluation to the console by passing in the value stored in 'hilarious' inside the .format string method.
+
+# store string in variable w
+w = "This is the left sid of..."
+# store string in variable e
+e = "a string with a right side."
+
+
+print(w+e) # concatenate the values of w and e and print to the console.
+
