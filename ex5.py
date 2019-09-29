91853f9c72dbf2c8b90cad69779485f9d730273b
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-28 23:41:22 -0700

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,22 @@
+my_name = "Nelson A. J."
+my_age = 28 # Fucking True
+my_height = 68 # Inches
+my_height_cm = round(my_height * 2.54)
+my_eyes = 'dark-brown'
+my_weight = 155 #lbs
+my_weight_kg = round(my_weight * 0.454)
+my_teeth = 'white'
+my_hair = 'black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"In centimeter he is {my_height_cm} centimeters.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"In kilograms he is {my_weight_kg} kilograms.")
+print(f"ACtually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# Add the values in my_age, my_height, and my_weight
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
