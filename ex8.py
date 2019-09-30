2c89b3aee16a288afb9130cbca84655b0d9180b3
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-30 13:18:11 -0700

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# print more using the .format() method.
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+  "Nade desnudo tu oleaje",
+  "El aullido vertical, como respuesta a tus prodigios",
+  "La ficcion es y sera mi unica realidad",
+  "La espuma de venus"
+))
