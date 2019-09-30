6391ccdc2932a0d88388e3473fd087ba421efb60
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-30 13:42:49 -0700

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,22 @@
+tabby_cat = "\tI'm tabbed in." # adds space before it
+persian_cat = "I'm split\non a line." # split string in two lines.
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+current_song = "Nuestros Nombres"
+band_name = "Heroes Del Silencio"
+lyrics = """
+\t Y con grito\n\t de esperanza\n\t te digo adios\n
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(current_song)
+print(lyrics)
+print(current_song + " by " + band_name + "\n" + lyrics)
