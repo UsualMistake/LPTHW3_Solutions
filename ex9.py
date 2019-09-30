9e9b93238f1387ec8f4ec5da3bc751cc02af796d
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-30 13:27:02 -0700

    create ex9.py

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMarch\nApril\nMay\nJune\nJuly\nAug" #\n inserts a new line
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# triple-quotes allow you to print mulple lines to the console.
+
+print("""
+  Using the triple-quotes
+  to print to the console
+  allows you to write mulple
+  lines if we want to.
+  How awesome it all is""")
