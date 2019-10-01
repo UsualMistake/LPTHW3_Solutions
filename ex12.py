7c6ac122b08f1fed0603d5c67470cfbbed53a5db
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-09-30 17:19:17 -0700

    create ex12.py

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#re-write ex11.py using the input() only
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So you're {age} years old, {height} inches tall and {weight} lbs heavy.")
